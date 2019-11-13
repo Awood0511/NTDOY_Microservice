e898759079e8faebee40a71dc5a715e9c63b0db2
Adam Wood <adamtylerwood0511@gmail.com>
2019-11-13 16:11:32 -0500

    add test solution

---
+++ NTDOY_Microservice_Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace NTDOY_Microservice_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
